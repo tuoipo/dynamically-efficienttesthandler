9d486ab24f7b4c77a2ce88cb730ed85595426751
tuoipo <maddog0185nam1311@outlook.com>
2024-05-07 09:00:12 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
+function searchRange(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let start = -1;
+  let end = -1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      start = mid;
+      end = mid;
+      while (nums[start] === target) start--;
+      while (nums[end] === target) end++;
+      return [start + 1, end - 1];
+    } else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
   }
-  return i === s.length;
+  return [start, end];
 }
