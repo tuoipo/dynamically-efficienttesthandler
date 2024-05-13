6fad57d55f2069f9d0663b5911b37e89416b2f8d
tuoipo <maddog0185nam1311@outlook.com>
2024-05-14 00:34:04 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
+  }
+  return [...map.values()];
 }
